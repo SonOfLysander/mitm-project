d40cbb4159c80e9aed66da143a6bf379240dccf8
Paul Baker <paul.baker@overstock.com>
2015-09-30 16:30:35 -0600

    Adding simple spring project.

---
+++ mitm/src/main/java/io/paulbaker/mitm/SpringMitmApplication.java
@@ -0,0 +1,12 @@
+package io.paulbaker.mitm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMitmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMitmApplication.class, args);
+  }
+}
